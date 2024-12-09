3f9a4ab84cf0c235397e6da9aa9bb2b43ff58356
Parvez Mayar <parvezbaloc32@gmail.com>
2024-12-09 21:01:04 +0500

    Navigation logics improvement.

--- app/src/main/java/com/codemaster/codemasterapp/main/ui/bottomNavigation/navgraph/mainNavHost.kt
+++ app/src/main/java/com/codemaster/codemasterapp/main/ui/bottomNavigation/navgraph/mainNavHost.kt
@@ -1,9 +1,15 @@
 package com.codemaster.codemasterapp.main.ui.bottomNavigation.navgraph
 
+
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
-import androidx.navigation.navigation
+import androidx.navigation.compose.navigation
 import com.codemaster.codemasterapp.main.DataBase.NoteViewModel
 import com.codemaster.codemasterapp.main.ui.bottomNavigation.navgraph.routes.MainRoutes
 import com.codemaster.codemasterapp.main.ui.learning.lessons.LessonListScreen
@@ -20,15 +26,70 @@ fun NavGraphBuilder.mainNavHost(
     navigation(
         startDestination = MainRoutes.LevelSelectionScreen.route,
         route = MainRoutes.MAIN_ROOT.route
-    ) {
-        composable(MainRoutes.LevelSelectionScreen.route) {
+    )
+    {
+
+        composable(MainRoutes.LevelSelectionScreen.route,
+            enterTransition = {
+                scaleIn(
+                    initialScale = 0.95f,
+                    animationSpec = tween(160)
+                ) + fadeIn(animationSpec = tween(100))
+            },
+            exitTransition = {
+                scaleOut(
+                    targetScale = .95f,
+                    animationSpec = tween(160)
+                ) + fadeOut(animationSpec = tween(150))
+            },
+            popEnterTransition = {
+                scaleIn(initialScale = .95f, animationSpec = tween(160)) + fadeIn(
+                    animationSpec = tween(
+                        100
+                    )
+                )
+            },
+            popExitTransition = {
+                scaleOut(
+                    targetScale = .95f,
+                    animationSpec = tween(160)
+                ) + fadeOut(animationSpec = tween(150))
+            }
+        ) {
             LevelSelectionScreen(
                 navController = navController,
                 courseViewModel = courseViewModel,
                 noteViewModel = noteViewModel
             )
         }
-        composable(MainRoutes.LessonListScreen.route) {
+
+        composable(MainRoutes.LessonListScreen.route,
+            enterTransition = {
+                scaleIn(
+                    initialScale = 0.95f,
+                    animationSpec = tween(160)
+                ) + fadeIn(animationSpec = tween(100))
+            },
+            exitTransition = {
+                scaleOut(
+                    targetScale = .95f,
+                    animationSpec = tween(160)
+                ) + fadeOut(animationSpec = tween(150))
+            },
+            popEnterTransition = {
+                scaleIn(initialScale = .95f, animationSpec = tween(160)) + fadeIn(
+                    animationSpec = tween(
+                        100
+                    )
+                )
+            },
+            popExitTransition = {
+                scaleOut(
+                    targetScale = .95f,
+                    animationSpec = tween(160)
+                ) + fadeOut(animationSpec = tween(150))
+            }
+        ) {
             LessonListScreen(
                 navController = navController,
                 courseViewModel = courseViewModel,
@@ -36,7 +97,33 @@ fun NavGraphBuilder.mainNavHost(
             )
         }
 
-        composable(MainRoutes.LessonContentScreen.route) {
+        composable(MainRoutes.LessonContentScreen.route,
+            enterTransition = {
+                scaleIn(
+                    initialScale = 0.95f,
+                    animationSpec = tween(160)
+                ) + fadeIn(animationSpec = tween(100))
+            },
+            exitTransition = {
+                scaleOut(
+                    targetScale = .95f,
+                    animationSpec = tween(160)
+                ) + fadeOut(animationSpec = tween(150))
+            },
+            popEnterTransition = {
+                scaleIn(initialScale = .95f, animationSpec = tween(160)) + fadeIn(
+                    animationSpec = tween(
+                        100
+                    )
+                )
+            },
+            popExitTransition = {
+                scaleOut(
+                    targetScale = .95f,
+                    animationSpec = tween(160)
+                ) + fadeOut(animationSpec = tween(150))
+            }
+        ) {
             LessonContentScreen(
                 navController = navController,
                 courseViewModel = courseViewModel,
