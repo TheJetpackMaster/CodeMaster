2ebae3b8cf94a87122be4ddd5ab26f5d20525713
Parvez Mayar <parvezbaloc32@gmail.com>
2024-12-09 20:22:45 +0500

    Navigation logics improvement.

--- app/src/main/java/com/codemaster/codemasterapp/main/ui/bottomNavigation/navgraph/routes/BottomNavRoutes.kt
+++ app/src/main/java/com/codemaster/codemasterapp/main/ui/bottomNavigation/navgraph/routes/BottomNavRoutes.kt
@@ -2,7 +2,7 @@ package com.codemaster.codemasterapp.main.ui.bottomNavigation.navgraph.routes
 
 sealed class BottomNavRoutes(val route: String) {
     data object BOTTOM_ROOT : BottomNavRoutes(route = "BOTTOM_ROOT")
-    data object HomeScreen : BottomNavRoutes(route = "bottomsomescreen")
+    data object HomeScreen : BottomNavRoutes(route = "bottomhomescreen")
     data object CompilerScreen: BottomNavRoutes(route = "compilerscreen")
 
     data object AchievementsScreen: BottomNavRoutes(route = "bottomachievemementscreen")
