adacd5960df7e3135f176cf9887427e28eb24e7d
hP <balochkhubaib008@gmail.com>
2024-12-09 19:57:33 +0500

    Note Item Added.

--- app/src/main/java/com/codemaster/codemasterapp/main/ui/userProfileDetails/NoteScreen.kt
+++ app/src/main/java/com/codemaster/codemasterapp/main/ui/userProfileDetails/NoteScreen.kt
@@ -148,7 +148,7 @@ fun NoteScreen(
                     checkIcon = R.drawable.editicon,
                     editIcon = R.drawable.editicon,
                     editButtonEnabled = true,
-                    bgColor = Color(0xFF375A7F).copy(1f),
+                    bgColor = Color(0xFF375A7F).copy(0.5f),
                     subLesson = subLesson
                     )
 //                bgColor = Color(0xFF1E88E5).copy(.5f),
