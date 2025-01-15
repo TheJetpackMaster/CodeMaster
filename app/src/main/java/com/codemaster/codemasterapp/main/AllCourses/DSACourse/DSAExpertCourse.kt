b6917e6041f7c08f931594bfce45dc4ba9dfd647
hP <balochkhubaib008@gmail.com>
2025-01-15 13:28:47 +0500

    DSA in progress.

--- app/src/main/java/com/codemaster/codemasterapp/main/AllCourses/DSACourse/DSAExpertCourse.kt
+++ app/src/main/java/com/codemaster/codemasterapp/main/AllCourses/DSACourse/DSAExpertCourse.kt
@@ -271,10 +271,10 @@ fun DSAExpertCourse(): Stage {
                                 createAnnotatedText(
                                     """
                         Example:  
-                        **Input:** 'arr1 = [1, 3, 5]', 'arr2 = [2, 4, 6]'  
-                        **Output:** '[1, 2, 3, 4, 5, 6]'  
+                        Input: arr1 = [1, 3, 5], arr2 = [2, 4, 6] 
+                        Output: [1, 2, 3, 4, 5, 6]' 
                         """.trimIndent(),
-                                    listOf("example", "input", "output")
+                                    listOf("")
                                 )
                             )
                         ),
